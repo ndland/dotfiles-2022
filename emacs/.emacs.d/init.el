2ba23c464d46b39e8d85f352165ae774d0fff3d7
Nick <Land>
2021-01-14 10:56:57 -0500

    ➕ Add Straight.el

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -25,12 +25,25 @@
 (unless package-archive-contents
   (package-refresh-contents))
 
-;; Initialize use-package
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
+;; Straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+;; (setq straight-use-package-by-default t)
 
 (require 'use-package)
-(setq use-package-always-ensure t)
+;; (setq use-package-always-ensure t)
 
 (column-number-mode)
 (global-display-line-numbers-mode t)
