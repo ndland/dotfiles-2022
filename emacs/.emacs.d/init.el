f2db5e96bacd31a7c4140aa481b90917d56b9759
Nick <Land>
2021-01-26 17:35:12 -0500

    🔧 Update auto-save and backup files for Emacs
    I don't want this stuff in my working directories.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -63,8 +63,15 @@
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
 
-;; Set backup directory
-(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+(setq
+ delete-old-versions t
+ kept-new-versions 6
+ kept-old-versions 2
+ version-control t
+ backup-directory-alist
+ `((".*" . ,temporary-file-directory))
+ auto-save-file-name-transforms
+ `((".*" ,temporary-file-directory t)))
 
 ;; This is needed as of Org 9.2
 (require 'org-tempo)
