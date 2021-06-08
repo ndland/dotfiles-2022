8dd38d786ffab156d828e21a9444550688cb7671
Nick Land <land.d.nicholas@gmail.com>
2021-06-08 12:01:14 -0400

    🔨 Fix org-mode
    This commit is too big for my liking, but here is a summary of what
    I've done:
    
    * Fixed org-mode, it is now running the latest stable version via
    straight
    * Cleaned up straight, init no longer references package.el
    * Fixed the way that doom-modeline is installed with straight

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,11 +1,7 @@
 ;; Initialize package sources
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("org" . "https://orgmode.org/elpa/")
-			 ("elpa" . "https://elpa.gnu.org/packages/")))
-
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
+(setq package-archives '(("org" . "https://orgmode.org/elpa/")
+                         ("melpa" . "https://melpa.org/packages/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
 
 ;; Straight.el
 (defvar bootstrap-version)
@@ -14,18 +10,19 @@
       (bootstrap-version 5))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
-	(url-retrieve-synchronously
-	 "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-	 'silent 'inhibit-cookies)
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
 (straight-use-package 'use-package)
+;; Ok, putting this here gives me the latest version
+(straight-use-package 'org)
 (setq straight-use-package-by-default t)
 
 (require 'use-package)
-(setq use-package-always-ensure t)
 
 (use-package spacemacs-theme
   :defer t)
@@ -55,12 +52,12 @@
 
 ;; Disable line numbers for some modes
 (dolist (mode '(org-mode-hook
-		term-mode-hook
-		vterm-mode-hook
-		nov-mode-hook
-		treemacs-mode-hook
-		org-agenda-mode-hook
-		eshell-mode-hook))
+                term-mode-hook
+                vterm-mode-hook
+                nov-mode-hook
+                treemacs-mode-hook
+                org-agenda-mode-hook
+                eshell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 (use-package rainbow-delimiters
@@ -77,13 +74,13 @@
 
 ;; Set the variable pitch face
 (set-face-attribute 'fixed-pitch nil
-		    :font "Iosevka Nerd Font Mono"
-		    :height 130)
+                    :font "Iosevka Nerd Font Mono"
+                    :height 130)
 
 ;; Set the variable pitch face
 (set-face-attribute 'variable-pitch nil
-		    :font "Iosevka Etoile"
-		    :height 130)
+                    :font "Iosevka Etoile"
+                    :height 130)
 
 (setq
  delete-old-versions t
@@ -108,7 +105,7 @@
 ;; Automatically tangle our Emacs.org config file when we save it
 (defun nl/org-babel-tangle-config ()
   (when (string-equal (file-name-directory (buffer-file-name))
-		      nl/emacs-dotfile-directory)
+                      nl/emacs-dotfile-directory)
     ;; Dynamic scoping to the rescue
     (let ((org-confirm-babel-evaluate nil))
       (org-babel-tangle))))
@@ -182,21 +179,185 @@
     "b" '(:ignore t :which-key "Buffers")
     "bb" '(counsel-ibuffer :which-key "Switch Buffers")))
 
+(defun nl/org-mode-setup ()
+  (variable-pitch-mode 1)
+  (auto-fill-mode 1))
+
+(defun nl/org-heading-setup ()
+  ;; Scale headings
+  (dolist (face '((org-level-1 . 1.5)
+                  (org-level-2 . 1.1)
+                  (org-level-3 . 1.05)
+                  (org-level-4 . 1.1)
+                  (org-level-5 . 1.1)
+                  (org-level-6 . 1.1)
+                  (org-level-7 . 1.1)
+                  (org-level-8 . 1.1)))
+    (set-face-attribute (car face) nil :font "Iosevka Etoile" :weight 'normal :height (cdr face))))
+
+(require 'org-habit)
+(add-to-list 'org-modules 'org-habit)
+
+(use-package org
+  :hook
+  ((org-mode . nl/org-mode-setup)
+   (org-mode . org-indent-mode))
+  :bind
+  ([remap org-set-tags-command] . #'counsel-org-tag)
+  :config
+  (setq org-log-into-drawer t)
+  ;; (org-hide-drawer-toggle t)
+  (setq org-agenda-files '("~/Dropbox/org/inbox.org"))
+  (setq org-contacts-files '("~/Dropbox/org/inbox.org"))
+  ;; (setq org-agenda-include-diary t)
+
+  (setq org-ellipsis " ⌄"
+        org-hide-emphasis-markers t)
+  ;; (nl/org-heading-setup)
+
+  (setq org-habit-graph-column 60)
+  (setq org-tags-column 120
+        org-auto-align-tags t)
+  (setq org-startup-with-inline-images t
+        org-image-actual-width 500)
+
+  (set-face-attribute 'org-block nil    :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-table nil    :inherit 'fixed-pitch)
+  (set-face-attribute 'org-date nil     :inherit 'fixed-pitch)
+  (set-face-attribute 'org-link nil     :inherit 'fixed-pitch)
+  (set-face-attribute 'org-checkbox-statistics-todo nil :inherit 'fixed-pitch :foreground "DarkOrange1")
+  (set-face-attribute 'org-checkbox-statistics-done nil :inherit 'fixed-pitch :foreground "lime green")
+  (set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil     :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-table nil    :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil  :inherit 'fixed-pitch)
+  (set-face-attribute 'org-agenda-calendar-sexp nil :inherit 'variable-pitch :foreground "LemonChiffon")
+
+  (setq org-refile-targets '((org-agenda-files :maxlevel . 3)))
+
+  (setq org-outline-path-complete-in-steps nil)
+  (setq org-refile-use-outline-path t)
+
+  (setq org-todo-keywords
+        '((sequence "TODO(t)" "NEXT(n!)" "|" "DONE(d!)")
+          (sequence "BACKLOG(b!)" "PLAN(p!)" "READY(r!)" "ACTIVE(a!)" "REVIEW(e!)" "WAITING(w@/!)" "HOLD(h@/!)" "|" "COMPLETED(c!)" "CANCELLED(l@/!)")))
+
+  (setq org-todo-keyword-faces
+        '(("TODO" . org-warning)
+          ("NEXT" . (:foreground "gold" :weight bold))
+          ("DONE" . (:foreground "lime green" :weight bold))
+          ("BACKLOG" . (:foreground "dim gray" :weight regular))
+          ("PLAN" . (:foreground "orange red" :weight regular))
+          ("READY" . (:foreground "spring green" :weight bold))
+          ("ACTIVE" . (:foreground "yellow" :weight bold))
+          ("REVIEW" . (:foreground "orange" :weight bold))
+          ("WAITING" . (:foreground "salmon" :weight bold))
+          ("HOLD" . (:foreground "tomato" :weight bold))
+          ("COMPLETED" . (:foreground "lime green" :weight bold))
+          ("CANCELLED" . (:foreground "red" :weight bold))))
+
+  (setq org-capture-templates
+        '(("t" "Tasks")
+          ("tt" "Task" entry
+           (file+olp "~/Dropbox/org/inbox.org" "Inbox")
+           "* TODO %?\n  Captured: %U\n  %a\n %i"
+           :empty-lines 0)
+          ("td" "Task Today" entry
+           (file+olp "~/Dropbox/org/inbox.org" "Inbox")
+           "* TODO %?\nSCHEDULED: %t\n  Captured: %U\n  %a\n %i"
+           :empty-lines 0)
+          ("c" "Contacts")
+          ("cf" "Family" entry (file+headline "~/Dropbox/org/inbox.org" "Family")
+           "* %(org-contacts-template-name)
+:PROPERTIES:
+:ADDRESS: %^{289 Cleveland St. Brooklyn, 11206 NY, USA}
+:BIRTHDAY: %^{yyyy-mm-dd}
+:EMAIL: %^{Email}
+:PHONE:
+:ALIAS:
+:NICKNAME:
+:IGNORE:
+:ICON:
+:NOTE: %^{Note}
+:END:"
+           :empty-lines 0)
+          ("cr" "Friends" entry (file+olp "~/Dropbox/org/inbox.org" "Contacts" "Friends")
+           "* %(org-contacts-template-name)
+:PROPERTIES:
+:ADDRESS: %^{289 Cleveland St. Brooklyn, 11206 NY, USA}
+:BIRTHDAY: %^{yyyy-mm-dd}
+:EMAIL: %^{Email}
+:PHONE:
+:ALIAS:
+:NICKNAME:
+:IGNORE:
+:ICON:
+:NOTE: %^{Note}
+:END:"
+           :empty-lines 0)))
+
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((emacs-lisp . t)
+     (C . t)))
+
+  (setq org-tag-alist
+        '((:startgroup)
+          (:endgroup)
+          ("@home" . ?H)
+          ("@errand" . ?E)
+          ("@work" . ?W)
+          ("finance" . ?F)
+          ("event" . ?v)
+          ("habit" . ?a)
+          ("chore" . ?C)
+          ("plex" . ?P)
+          ("hobbies" . ?h)
+          ("productivity" . ?p)
+          ("emacs" . ?e)
+          ("repair" . ?r)))
+
+  (general-define-key
+   :states '(normal insert visual emacs)
+   :keymaps 'org-agenda-mode-map
+   "j" 'org-agenda-next-line
+   "k" 'org-agenda-previous-line))
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode))
+
+(defun nl/org-mode-visual-fill ()
+  (setq visual-fill-column-width 120
+        visual-fill-column-center-text t)
+  (visual-fill-column-mode 1))
+
+(use-package visual-fill-column
+  :hook (org-mode . nl/org-mode-visual-fill))
+
+(use-package org-contrib
+  :after org
+  :config
+  (require 'org-contacts))
+
 (use-package ivy
   :diminish
   :bind (("C-s" . swiper)
-	 :map ivy-minibuffer-map
-	 ("TAB" . ivy-alt-done)
-	 ("C-l" . ivy-alt-done)
-	 ("C-j" . ivy-next-line)
-	 ("C-k" . ivy-previous-line)
-	 :map ivy-switch-buffer-map
-	 ("C-k" . ivy-previous-line)
-	 ("C-l" . ivy-done)
-	 ("C-d" . ivy-switch-buffer-kill)
-	 :map ivy-reverse-i-search-map
-	 ("C-k" . ivy-previous-line)
-	 ("C-d" . ivy-reverse-i-search-kill))
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
   :init
   (ivy-mode 1)
   :config
@@ -208,11 +369,11 @@
 
 (use-package counsel
   :bind (("M-x" . counsel-M-x)
-	 ("C-x b" . counsel-ibuffer)
-	 ("C-x C-f" . counsel-find-file)
-	 ("C-x C-r" . counsel-recentf)
-	 :map minibuffer-local-map
-	 ("C-r" . 'counsel-minibuffer-history))
+         ("C-x b" . counsel-ibuffer)
+         ("C-x C-f" . counsel-find-file)
+         ("C-x C-r" . counsel-recentf)
+         :map minibuffer-local-map
+         ("C-r" . 'counsel-minibuffer-history))
   :init (counsel-mode 1))
 
 (use-package helpful
@@ -226,7 +387,8 @@
   ([remap describe-key] . helpful-key))
 
 (use-package doom-modeline
-  :init (doom-modeline-mode 1))
+  :ensure t
+  :hook (after-init . doom-modeline-mode))
 
 (use-package all-the-icons)
 
@@ -261,9 +423,9 @@
   :hook (after-init . global-company)
   :bind
   (:map company-active-map
-	("<tab>" . company-complete-selection))
+        ("<tab>" . company-complete-selection))
   (:map lsp-mode-map
-	("<tab>" . company-indent-or-complete-common))
+        ("<tab>" . company-indent-or-complete-common))
   :custom
   (company-minimum-prefix-length 1)
   (company-idle-delay 0.0))
@@ -279,11 +441,11 @@
   :config
   (global-flycheck-mode)
   (setq-default flycheck-disabled-checkers
-		(append flycheck-disabled-checkers
-			'(javascript-jshint)))
+                (append flycheck-disabled-checkers
+                        '(javascript-jshint)))
   (setq-default flycheck-disabled-checkers
-		(append flycheck-disabled-checkers
-			'(json-jsonlist)))
+                (append flycheck-disabled-checkers
+                        '(json-jsonlist)))
   (flycheck-add-mode 'javascript-eslint 'web-mode))
 
 (use-package yasnippet
@@ -344,8 +506,8 @@
 (use-package markdown-mode
   :commands (markdown-mode gfm-mode)
   :mode (("README\\.md\\'" . gfm-mode)
-	 ("\\.md\\'" . markdown-mode)
-	 ("\\.markdown\\'" . markdown-mode))
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
   :init (setq markdown-command "pandoc"))
 
 (use-package json-mode
@@ -355,10 +517,10 @@
 
 (use-package web-mode
   :mode (("\\.js\\'" . web-mode)
-	 ("\\.jsx\\'" . web-mode)
-	 ("\\.ts\\'" . web-mode)
-	 ("\\.html\\'" . web-mode)
-	 ("\\.tsx\\'" . web-mode))
+         ("\\.jsx\\'" . web-mode)
+         ("\\.ts\\'" . web-mode)
+         ("\\.html\\'" . web-mode)
+         ("\\.tsx\\'" . web-mode))
   :hook ((web-mode . lsp-deferred))
   :config
   (setq company-tooltip-align-annotations t)
@@ -366,7 +528,7 @@
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2)
   (setq web-mode-content-types-alist
-	'(("jsx" . "\\.js[x]?\\'"))))
+        '(("jsx" . "\\.js[x]?\\'"))))
 
 
 (use-package prettier-js
@@ -374,9 +536,9 @@
 
 (use-package beancount-mode
   :straight (beancount-mode
-	     :type git
-	     :host github
-	     :repo "beancount/beancount-mode")
+             :type git
+             :host github
+             :repo "beancount/beancount-mode")
   :hook
   (beancount-mode . outline-minor)
   :bind
@@ -404,163 +566,5 @@
 (use-package yaml-mode
   :config
   (add-hook 'yaml-mode-hook
-	    (lambda ()
-	      (define-key yaml-mode-map "\C-m" 'newline-and-indent))))
-
-(defun nl/org-mode-setup ()
-  (variable-pitch-mode 1)
-  (auto-fill-mode 1))
-
-(defun nl/org-heading-setup ()
-  ;; Scale headings
-  (dolist (face '((org-level-1 . 1.5)
-		  (org-level-2 . 1.1)
-		  (org-level-3 . 1.05)
-		  (org-level-4 . 1.1)
-		  (org-level-5 . 1.1)
-		  (org-level-6 . 1.1)
-		  (org-level-7 . 1.1)
-		  (org-level-8 . 1.1)))
-    (set-face-attribute (car face) nil :font "Iosevka Etoile" :weight 'normal :height (cdr face))))
-
-(require 'org-habit)
-(add-to-list 'org-modules 'org-habit)
-
-(use-package org
-  :hook (org-mode . nl/org-mode-setup)
-  :bind
-  ([remap org-set-tags-command] . #'counsel-org-tag)
-  :config
-  (setq org-log-into-drawer t)
-  ;; (org-hide-drawer-toggle t)
-  (setq org-agenda-files '("~/Dropbox/org/inbox.org"))
-
-  (setq org-ellipsis " ⌄"
-	org-hide-emphasis-markers t)
-  ;; (nl/org-heading-setup)
-
-  (setq org-habit-graph-column 60)
-  (setq org-tags-column 120
-	org-auto-align-tags t)
-  (setq org-startup-with-inline-images t
-	org-image-actual-width 500)
-
-  (set-face-attribute 'org-block nil    :foreground nil :inherit 'fixed-pitch)
-  (set-face-attribute 'org-table nil    :inherit 'fixed-pitch)
-  (set-face-attribute 'org-date nil     :inherit 'variable-pitch)
-  (set-face-attribute 'org-link nil     :inherit 'fixed-pitch)
-  (set-face-attribute 'org-checkbox-statistics-todo nil :inherit 'fixed-pitch :foreground "DarkOrange1")
-  (set-face-attribute 'org-checkbox-statistics-done nil :inherit 'fixed-pitch :foreground "lime green")
-  (set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
-  (set-face-attribute 'org-code nil     :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-table nil    :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
-  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
-  (set-face-attribute 'org-checkbox nil  :inherit 'fixed-pitch)
-
-  (setq org-refile-targets '((org-agenda-files :maxlevel . 3)))
-
-  (setq org-outline-path-complete-in-steps nil)
-  (setq org-refile-use-outline-path t)
-
-  (setq org-todo-keywords
-	'((sequence "TODO(t)" "NEXT(n!)" "|" "DONE(d!)")
-	  (sequence "BACKLOG(b!)" "PLAN(p!)" "READY(r!)" "ACTIVE(a!)" "REVIEW(e!)" "WAITING(w@/!)" "HOLD(h@/!)" "|" "COMPLETED(c!)" "CANCELLED(l@/!)")))
-
-  (setq org-todo-keyword-faces
-	'(("TODO" . org-warning)
-	  ("NEXT" . (:foreground "gold" :weight bold))
-	  ("DONE" . (:foreground "lime green" :weight bold))
-	  ("BACKLOG" . (:foreground "dim gray" :weight regular))
-	  ("PLAN" . (:foreground "orange red" :weight regular))
-	  ("READY" . (:foreground "spring green" :weight bold))
-	  ("ACTIVE" . (:foreground "yellow" :weight bold))
-	  ("REVIEW" . (:foreground "orange" :weight bold))
-	  ("WAITING" . (:foreground "salmon" :weight bold))
-	  ("HOLD" . (:foreground "tomato" :weight bold))
-	  ("COMPLETED" . (:foreground "lime green" :weight bold))
-	  ("CANCELLED" . (:foreground "red" :weight bold))))
-
-  (setq org-capture-templates
-	'(("b" "Bookmarks" entry
-	   (file+olp "~/Dropbox/org/bookmarks.org" "Bookmarks")
-	   "* %?\n:PROPERTIES:\n:CREATED: %u\n:END:\n  %a\n %i"
-	   :empty-lines 0)
-	  ("t" "Tasks")
-	  ("tt" "Task" entry
-	   (file+olp "~/Dropbox/org/tasks.org" "Inbox")
-	   "* TODO %?\nCaptured: %U\n  %a\n %i"
-	   :empty-lines 0)
-	  ("td" "Task Today" entry
-	   (file+olp "~/Dropbox/org/tasks.org" "Inbox")
-	   "* TODO %?\nSCHEDULED: %t\nCaptured: %U\n  %a\n %i"
-	   :empty-lines 0)
-	  ("j" "Journal")
-	  ("jj" "Journal" entry
-	   (file+olp+datetree "~/Dropbox/org/journal.org" "Journal")
-	   "\n* %<%I:%M %p> - %^{Summary} :journal:\n\n%?\n"
-	   :empty-lines 0 :clock-in :clock-resume)
-	  ("ja" "AutoCAD" entry
-	   (file+olp+datetree "~/Dropbox/org/journal.org" "AutoCAD")
-	   "\n* %<%I:%M %p> - %^{Summary} :journal:autocad:\n\n%?\n"
-	   :empty-lines 0 :clock-in :clock-resume)
-	  ("jb" "Blender" entry
-	   (file+olp+datetree "~/Dropbox/org/journal.org" "Blender")
-	   "\n* %<%I:%M %p> - %^{Summary} :journal:blender:\n\n%?\n"
-	   :empty-lines 0 :clock-in :clock-resume)
-	  ("je" "Exercise" entry
-	   (file+olp+datetree "~/Dropbox/org/journal.org" "Exercise")
-	   "\n* %<%I:%M %p> - %^{Summary} :journal:exercise:\n\n%?\n"
-	   :empty-lines 0 :clock-in :clock-resume)
-	  ("jp" "Programming" entry
-	   (file+olp+datetree "~/Dropbox/org/journal.org" "Programming")
-	   "\n* %<%I:%M %p> - %^{Summary} :journal:programming:\n\n%?\n"
-	   :empty-lines 0 :clock-in :clock-resume)
-	  ("jg" "Guitar" entry
-	   (file+olp+datetree "~/Dropbox/org/journal.org" "Guitar")
-	   "\n* %<%I:%M %p> - %^{Summary} :journal:guitar:\n\n%?\n"
-	   :empty-lines 0 :clock-in :clock-resume)
-	  ("jr" "Revit" entry
-	   (file+olp+datetree "~/Dropbox/org/journal.org" "Revit")
-	   "\n* %<%I:%M %p> - %^{Summary} :journal:revit:\n\n%?\n"
-	   :empty-lines 0 :clock-in :clock-resume)))
-
-  (org-babel-do-load-languages
-   'org-babel-load-languages
-   '((emacs-lisp . t)
-     (C . t)))
-
-  (setq org-tag-alist
-	'((:startgroup)
-	  (:endgroup)
-	  ("@home" . ?H)
-	  ("@errand" . ?E)
-	  ("@work" . ?W)
-	  ("finance" . ?F)
-	  ("event" . ?v)
-	  ("habit" . ?a)
-	  ("chore" . ?C)
-	  ("plex" . ?P)
-	  ("hobbies" . ?h)
-	  ("productivity" . ?p)
-	  ("emacs" . ?e)
-	  ("repair" . ?r)))
-
-  (general-define-key
-   :states '(normal insert visual emacs)
-   :keymaps 'org-agenda-mode-map
-   "j" 'org-agenda-next-line
-   "k" 'org-agenda-previous-line))
-
-(use-package org-bullets
-  :after org
-  :hook (org-mode . org-bullets-mode))
-
-(defun nl/org-mode-visual-fill ()
-  (setq visual-fill-column-width 120
-	visual-fill-column-center-text t)
-  (visual-fill-column-mode 1))
-
-(use-package visual-fill-column
-  :hook (org-mode . nl/org-mode-visual-fill))
+            (lambda ()
+              (define-key yaml-mode-map "\C-m" 'newline-and-indent))))
