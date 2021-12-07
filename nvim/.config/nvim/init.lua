d9101d578b03ea75adeae57c6f4b84931283241f
Nick Land <land.d.nicholas@gmail.com>
2021-12-07 12:17:46 -0500

    🎨 Set shiftwidth = 2

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -36,118 +36,122 @@ neogit.setup {}
 require('github-theme').setup()
 require('nvim-web-devicons').get_icons()
 
+-- Language settings
+opt.tabstop = 2
+opt.shiftwidth = 2
+
 -- Mapings
 -- Map ,n to toggle nvimTree
 map('n', '<leader>n', ':NvimTreeToggle<CR>', {})
 map('n', '<leader>g', ':Neogit<CR>', {})
 
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-  ignore_install = { }, -- List of parsers to ignore installing
-  highlight = {
-    enable = true,              -- false will disable the whole extension
-    disable = { },  -- list of language that will be disabled
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
-  indent = {
-    enable = true,              -- false will disable the whole extension
-  }
+	ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
+	ignore_install = { }, -- List of parsers to ignore installing
+	highlight = {
+		enable = true,              -- false will disable the whole extension
+		disable = { },  -- list of language that will be disabled
+		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+		-- Using this option may slow down your editor, and you may see some duplicate highlights.
+		-- Instead of true it can also be a list of languages
+		additional_vim_regex_highlighting = false,
+	},
+	indent = {
+		enable = true,              -- false will disable the whole extension
+	}
 }
 
 -- following options are the default
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
 require'nvim-tree'.setup {
-  disable_netrw       = true,
-  hijack_netrw        = true,
-  open_on_setup       = false,
-  ignore_ft_on_setup  = {},
-  auto_close          = false,
-  open_on_tab         = false,
-  hijack_cursor       = false,
-  update_cwd          = false,
-  update_to_buf_dir   = {
-    enable = true,
-    auto_open = true,
-  },
-  diagnostics = {
-    enable = false,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
-    }
-  },
-  update_focused_file = {
-    enable      = false,
-    update_cwd  = false,
-    ignore_list = {}
-  },
-  system_open = {
-    cmd  = nil,
-    args = {}
-  },
-  filters = {
-    dotfiles = true,
-    custom = {}
-  },
-  git = {
-    enable = true,
-    ignore = true,
-    timeout = 500,
-  },
-  view = {
-    width = 30,
-    height = 30,
-    hide_root_folder = false,
-    side = 'left',
-    auto_resize = false,
-    mappings = {
-      custom_only = false,
-      list = {}
-    },
-    number = false,
-    relativenumber = false
-  },
-  trash = {
-    cmd = "trash",
-    require_confirm = true
-  }
+	disable_netrw       = true,
+	hijack_netrw        = true,
+	open_on_setup       = false,
+	ignore_ft_on_setup  = {},
+	auto_close          = false,
+	open_on_tab         = false,
+	hijack_cursor       = false,
+	update_cwd          = false,
+	update_to_buf_dir   = {
+		enable = true,
+		auto_open = true,
+	},
+	diagnostics = {
+		enable = false,
+		icons = {
+			hint = "",
+			info = "",
+			warning = "",
+			error = "",
+		}
+	},
+	update_focused_file = {
+		enable      = false,
+		update_cwd  = false,
+		ignore_list = {}
+	},
+	system_open = {
+		cmd  = nil,
+		args = {}
+	},
+	filters = {
+		dotfiles = true,
+		custom = {}
+	},
+	git = {
+		enable = true,
+		ignore = true,
+		timeout = 500,
+	},
+	view = {
+		width = 30,
+		height = 30,
+		hide_root_folder = false,
+		side = 'left',
+		auto_resize = false,
+		mappings = {
+			custom_only = false,
+			list = {}
+		},
+		number = false,
+		relativenumber = false
+	},
+	trash = {
+		cmd = "trash",
+		require_confirm = true
+	}
 }
 
 -- Lualine config
 require'lualine'.setup {
-  options = {
-    theme = 'github',
-    icons_enabled = true,
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = {},
-    always_divide_middle = true,
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff',
-                  {'diagnostics', sources={'nvim_lsp', 'coc'}}},
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  extensions = {}
+	options = {
+		theme = 'github',
+		icons_enabled = true,
+		component_separators = { left = '', right = ''},
+		section_separators = { left = '', right = ''},
+		disabled_filetypes = {},
+		always_divide_middle = true,
+	},
+	sections = {
+		lualine_a = {'mode'},
+		lualine_b = {'branch', 'diff',
+			{'diagnostics', sources={'nvim_lsp', 'coc'}}},
+		lualine_c = {'filename'},
+		lualine_x = {'encoding', 'fileformat', 'filetype'},
+		lualine_y = {'progress'},
+		lualine_z = {'location'}
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = {'filename'},
+		lualine_x = {'location'},
+		lualine_y = {},
+		lualine_z = {}
+	},
+	tabline = {},
+	extensions = {}
 }
 
