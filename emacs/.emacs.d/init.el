eafa3334b25426cb2a129e6aedd265265c407b46
Nick Land <land.d.nicholas@gmail.com>
2022-02-15 19:17:09 -0500

    ✨ Add previous org-mode config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -44,7 +44,152 @@
 ;; Packages
 
 ;; Latest version of org-mode
-(use-package org)
+(defvar nl/org-directory "~/dev/github.com/ndland/org/")
+
+(defun nl/org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (auto-fill-mode 1))
+
+(use-package org
+  :hook (org-mode . nl/org-mode-setup)
+  :config
+  (setq org-log-into-drawer t)
+  (setq org-agenda-files (list (concat nl/org-directory "inbox.org")))
+  (setq org-contacts-files (list (concat nl/org-directory "inbox.org")))
+
+  (setq org-ellipsis " ⌄"
+        org-hide-emphasis-markers t)
+
+  (setq org-habit-graph-column 60)
+  (setq org-tags-column 120
+        org-auto-align-tags t)
+  (setq org-startup-with-inline-images t
+        org-image-actual-width 500)
+
+  (set-face-attribute 'org-block nil    :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-table nil    :inherit 'fixed-pitch)
+  (set-face-attribute 'org-date nil     :inherit 'fixed-pitch)
+  (set-face-attribute 'org-link nil     :inherit 'fixed-pitch)
+  (set-face-attribute 'org-checkbox-statistics-todo nil :inherit 'fixed-pitch :foreground "DarkOrange1")
+  (set-face-attribute 'org-checkbox-statistics-done nil :inherit 'fixed-pitch :foreground "lime green")
+  (set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil     :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-table nil    :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil  :inherit 'fixed-pitch)
+  (set-face-attribute 'org-agenda-calendar-sexp nil :inherit 'variable-pitch :foreground "LemonChiffon")
+
+  (setq org-refile-targets '((org-agenda-files :maxlevel . 3)))
+
+  (setq org-outline-path-complete-in-steps nil)
+  (setq org-refile-use-outline-path t)
+
+  (setq org-todo-keywords
+        '((sequence "TODO(t)" "NEXT(n!)" "|" "DONE(d!)")
+          (sequence "BACKLOG(b!)" "PLAN(p!)" "READY(r!)" "ACTIVE(a!)" "REVIEW(e!)" "WAITING(w@/!)" "HOLD(h@/!)" "|" "COMPLETED(c!)" "CANCELLED(l@/!)")))
+
+  (setq org-todo-keyword-faces
+        '(("TODO" . (:foreground "red" :weight bold))
+          ("NEXT" . (:foreground "gold" :weight bold))
+          ("DONE" . (:foreground "lime green" :weight bold))
+          ("BACKLOG" . (:foreground "dim gray" :weight regular))
+          ("PLAN" . (:foreground "orange red" :weight regular))
+          ("READY" . (:foreground "spring green" :weight bold))
+          ("ACTIVE" . (:foreground "yellow" :weight bold))
+          ("REVIEW" . (:foreground "orange" :weight bold))
+          ("WAITING" . (:foreground "salmon" :weight bold))
+          ("HOLD" . (:foreground "tomato" :weight bold))
+          ("COMPLETED" . (:foreground "lime green" :weight bold))
+          ("CANCELLED" . (:foreground "red" :weight bold))))
+
+  (setq org-capture-templates
+        '(("t" "Tasks")
+          ("tt" "Task" entry
+           (file+olp (concat nl/org-directory "inbox.org") "Inbox")
+           "* TODO %?\nCaptured: %U\n%a\n %i"
+           :empty-lines 0)
+          ("td" "Task Today" entry
+           (file+olp (concat nl/org-directory "inbox.org") "Inbox")
+           "* TODO %?\nSCHEDULED: %t\nCaptured: %U\n%a\n %i"
+           :empty-lines 0)
+          ("c" "Contacts")
+          ("cf" "Family" entry (file+headline (concat nl/org-directory "inbox.org") "Family")
+           "* %(org-contacts-template-name)
+:PROPERTIES:
+:ADDRESS: %^{289 Cleveland St. Brooklyn, 11206 NY, USA}
+:BIRTHDAY: %^{yyyy-mm-dd}
+:EMAIL: %^{Email}
+:PHONE:
+:ALIAS:
+:NICKNAME:
+:IGNORE:
+:ICON:
+:NOTE: %^{Note}
+:END:"
+           :empty-lines 0)
+          ("cr" "Friends" entry (file+olp (concat nl/org-directory "inbox.org") "Contacts" "Friends")
+           "* %(org-contacts-template-name)
+:PROPERTIES:
+:ADDRESS: %^{289 Cleveland St. Brooklyn, 11206 NY, USA}
+:BIRTHDAY: %^{yyyy-mm-dd}
+:EMAIL: %^{Email}
+:PHONE:
+:ALIAS:
+:NICKNAME:
+:IGNORE:
+:ICON:
+:NOTE: %^{Note}
+:END:"
+           :empty-lines 0)))
+
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((emacs-lisp . t)
+     (C . t)))
+
+  (setq org-tag-alist
+        '((:startgroup)
+          (:endgroup)
+          ("@home" . ?H)
+          ("@errand" . ?E)
+          ("@work" . ?W)
+          ("finance" . ?F)
+          ("event" . ?v)
+          ("habit" . ?a)
+          ("chore" . ?C)
+          ("plex" . ?P)
+          ("hobbies" . ?h)
+          ("productivity" . ?p)
+          ("emacs" . ?e)
+          ("repair" . ?r))))
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode))
+
+(defun nl/org-mode-visual-fill ()
+  (setq visual-fill-column-width 130
+        visual-fill-column-center-text t)
+  (visual-fill-column-mode 1))
+
+(use-package visual-fill-column
+  :hook (org-mode . nl/org-mode-visual-fill))
+
+(use-package org-contrib
+  :after org
+  :config
+  (require 'org-contacts))
+
+(use-package org-roam
+  :init
+  (setq org-roam-directory (file-truename "~/dev/github.com/ndland/org-roam"))
+  (org-roam-db-autosync-mode)
+  :bind (("C-c n t" . org-roam-buffer-toggle)
+	 ("C-c n f" . org-roam-node-find)
+	 ("C-c n i" . org-roam-node-insert)))
 
 ;; Evil; Vim bindings
 (use-package evil 
