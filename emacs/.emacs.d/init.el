f7c63a152090f126ad5ea9816eb6b038221b3f56
Nick Land <land.d.nicholas@gmail.com>
2022-08-08 21:56:44 -0400

    Fix issue with projectile not remembering previously visited projects
    I had an issue where projectile wouldn't remember projects between
    sessions. This change fixes that issue.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -113,8 +113,12 @@
   (global-set-key (kbd "C-h C") #'helpful-command))
 
 (use-package projectile
-  :config
-  (setq projectile-mode t))
+  :ensure t
+  :init
+  (projectile-mode +1)
+  :bind (:map projectile-mode-map
+              ("s-p" . projectile-command-map)
+              ("C-c p" . projectile-command-map)))
 
 (use-package magit)
 
