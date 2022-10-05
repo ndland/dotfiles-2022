87da8545e0ce124b43d91b927dc6d402bb62c4d0
Nick Land <land.d.nicholas@gmail.com>
2022-10-05 19:26:27 -0400

    emacs config updates
    * Not really sure what these are, but maybe one
    
    day I'll come back to them.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,6 +1,10 @@
 ;; Initialize Packages
 
 ;; Straight.el
+(setq package-archives '(("org" . "http://orgmode.org/elpa/")
+			 ("melpa" . "https://melpa.org/packages/")
+			 ("gnu" . "http://elpa.gnu.org/packages/")
+			 ("marmalade" . "https://marmalade-repo.org/packages/")))
 
 (defvar bootstrap-version)
 (let ((bootstrap-file
@@ -19,6 +23,9 @@
 (straight-use-package 'use-package)
 
 (straight-use-package 'org)
+
+(setq make-backup-files nil)
+(setq-default display-line-numbers 'visual)
 
 (use-package dracula-theme
   :config (load-theme 'dracula t))
@@ -142,3 +149,129 @@
 (use-package pinentry
   :ensure t
   :config (pinentry-start))
+
+(use-package helpful
+  :ensure t
+  :config
+  ;; Note that the built-in `describe-function' includes both functions
+  ;; and macros. `helpful-function' is functions only, so we provide
+  ;; `helpful-callable' as a drop-in replacement.
+  (global-set-key (kbd "C-h f") #'helpful-callable)
+
+  (global-set-key (kbd "C-h v") #'helpful-variable)
+  (global-set-key (kbd "C-h k") #'helpful-key)
+  ;; Lookup the current symbol at point. C-c C-d is a common keybinding
+  ;; for this in lisp modes.
+  (global-set-key (kbd "C-c C-d") #'helpful-at-point)
+
+  ;; Look up *F*unctions (excludes macros).
+  ;;
+  ;; By default, C-h F is bound to `Info-goto-emacs-command-node'. Helpful
+  ;; already links to the manual, if a function is referenced there.
+  (global-set-key (kbd "C-h F") #'helpful-function)
+
+  ;; Look up *C*ommands.
+  ;;
+  ;; By default, C-h C is bound to describe `describe-coding-system'. I
+  ;; don't find this very useful, but it's frequently useful to only
+  ;; look at interactive functions.
+  (global-set-key (kbd "C-h C") #'helpful-command))
+
+;; React from lsp tutoral
+(use-package lsp-mode
+  :ensure t)
+
+(use-package yasnippet
+  :ensure t)
+
+(use-package lsp-treemacs
+  :ensure t)
+
+(use-package projectile
+  :ensure t)
+
+(use-package hydra
+  :ensure t)
+
+(use-package flycheck
+  :ensure t)
+
+(use-package company
+  :ensure t
+  :config (global-company-mode +1))
+
+(use-package avy
+  :ensure t)
+
+(use-package which-key
+  :ensure t)
+
+(use-package dap-mode
+  :ensure t)
+
+(use-package json-mode
+  :ensure t)
+
+(use-package web-mode
+  :ensure t)
+
+(which-key-mode)
+(add-hook 'prog-mode-hook #'lsp)
+(setq gc-cons-threshold (* 100 1024 1024)
+      read-process-output-max (* 1024 1024)
+      company-idle-delay 0.0
+      company-minimum-prefix-length 1
+      create-lockfiles nil) ;; lock files will kill `npm start'
+(with-eval-after-load 'lsp-mode
+  (require 'dap-chrome)
+  (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
+  (yas-global-mode))
+
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+;; formats the buffer before saving
+(add-hook 'before-save-hook 'tide-format-before-save)
+
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "tsx" (file-name-extension buffer-file-name))
+              (setup-tide-mode))))
+;; enable typescript-tslint checker
+(flycheck-add-mode 'typescript-tslint 'web-mode)
+
+(use-package tide
+  :ensure t
+  :after (typescript-mode company flycheck)
+  :hook ((typescript-mode . tide-setup)
+         (typescript-mode . tide-hl-identifier-mode)
+         (before-save . tide-format-before-save)))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(warning-suppress-types '((lsp-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
